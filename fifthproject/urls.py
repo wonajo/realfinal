333eade9b2b9e615696b1026964a8eea070bd1c6
wonajo <wonajojo@gmail.com>
2021-06-28 18:01:27 +0900

    wow

--- fifthproject/urls.py
+++ fifthproject/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import dapp.views
 import accounts.views
+import photo.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',dapp.views.home, name="home"),
     path('d/<int:d_id>', dapp.views.detail, name='detail'),
-    path('accounts/', include('accounts.urls')),]
+    path('accounts/', include('accounts.urls')),
+    path('photo/',photo.views.photo,name='photo'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
